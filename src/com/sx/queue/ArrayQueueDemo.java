a53cb5d47f6ac35d578f77f360c7eff431baefb6
yfzr <sx19940306@outlook.com>
2020-06-09 22:57:31 +0800

    数组实现队列测试

--- src/com/sx/queue/ArrayQueueDemo.java
+++ src/com/sx/queue/ArrayQueueDemo.java
@@ -1,11 +1,56 @@
 package com.sx.queue;
 
 import javax.management.RuntimeErrorException;
+import java.util.Scanner;
 
 public class ArrayQueueDemo {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayQueue queue = new ArrayQueue(3);
+		Character key;
+		boolean loop = true;
+		while (loop){
+			System.out.println("s(show):显示队列");
+			System.out.println("e(exit):退出程序");
+			System.out.println("a(add):添加数据");
+			System.out.println("g(get):取出数据");
+			System.out.println("h(head):显示队列头");
 
+			key = scanner.next().charAt(0);
+			switch (key) {
+				case 's':
+					queue.showQueue();
+					break;
+				case 'e':
+					loop = false;
+					break;
+				case 'a':
+					System.out.println("请输入添加的数字：");
+					int value = scanner.nextInt();
+					queue.addQueue(value);
+					break;
+				case 'g':
+					try {
+						int data = queue.getQueue();
+						System.out.printf("取出的数据是：%d\n", data);
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+					break;
+				case 'h':
+					try {
+						int head = queue.headQueue();
+						System.out.printf("头部的数据是：%d\n", head);
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+					break;
+				default:
+					break;
+			}
+		}
+		System.out.println("已退出程序");
 	}
 
 	
@@ -15,12 +60,13 @@ public class ArrayQueueDemo {
  *使用数组模拟一个队列 
  */
 class ArrayQueue{
-	int maxSize;
-	int front;	//队列头
-	int rear;	//队列尾
+	private int maxSize;
+	private int front;	//队列头
+	private int rear;	//队列尾
 	private int[] arr;
 	//创建队列构造器
 	public ArrayQueue(int arrMaxSize) {
+		maxSize = arrMaxSize;
 		front = -1;
 		rear = -1;
 		arr = new int[arrMaxSize];
