656c4e30acd240dc5228ff756a34da58ff24d6bf
yfzr <sx19940306@outlook.com>
2020-06-12 22:38:12 +0800

    单链表的增删改实现

--- src/com/sx/linkedlist/SingleLinkedList.java
+++ src/com/sx/linkedlist/SingleLinkedList.java
@@ -7,13 +7,29 @@ public class SingleLinkedList {
         HeroNode hero2 = new HeroNode(2, "卢俊义", "玉麒麟");
         HeroNode hero3 = new HeroNode(3, "吴用", "智多星");
         HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
+        HeroNode hero41 = new HeroNode(4, "公孙胜", "入云龙");
+        HeroNode hero5 = new HeroNode(5, "张青", "菜园子");
 
         SingleLinkedListDemo slld = new SingleLinkedListDemo();
-        slld.add(hero1);
-        slld.add(hero2);
-        slld.add(hero3);
-        slld.add(hero4);
 
+        //测试添加方法1
+//        slld.add(hero1);
+//        slld.add(hero2);
+//        slld.add(hero3);
+//        slld.add(hero4);
+        //测试添加方法2
+        slld.addByOrder(hero1);
+        slld.addByOrder(hero4);
+        slld.addByOrder(hero3);
+        slld.addByOrder(hero2);
+        slld.addByOrder(hero2);
+
+        slld.list();
+        slld.update(hero41);
+
+        slld.list();
+
+        slld.delete(5);
         slld.list();
     }
 }
@@ -42,6 +58,87 @@ class SingleLinkedListDemo{
         //当退出循环时，temp为最后节点，需要将其指向新节点
         temp.next = heroNode;
     }
+
+    /**
+     * 当考虑按编号顺序添加
+     */
+    public void addByOrder(HeroNode heroNode){
+        HeroNode temp = head;
+        boolean flag = false;
+        while (true){
+            if (temp.next == null){ //说明英雄添加在最后
+                break;
+            }
+            if (temp.next.no > heroNode.no){
+                break;
+            }
+            if (temp.next.no == heroNode.no){   //存在重复编号
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag){
+            System.out.printf("准备插入的编号%d英雄已经存在，无法插入\n", heroNode.no);
+        }else {
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
+    }
+
+    //修改节点信息，但不能修改no，否则相当于添加
+    public void update(HeroNode newHeroNode){
+        HeroNode temp = head;
+        boolean flag = false;
+        if (head.next == null){
+            System.out.println("链表为空，无法修改");
+            return;
+        }
+        while (true){
+            if (temp == null){
+                break;
+            }
+            if (temp.no == newHeroNode.no){
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag){
+            temp.name = newHeroNode.name;
+            temp.nickName = newHeroNode.nickName;
+            System.out.printf("编号%d的英雄信息已被修改\n", newHeroNode.no);
+        }else {
+            System.out.printf("没有找到编号%d的节点，修改失败\n", newHeroNode.no);
+        }
+    }
+
+    //删除节点
+    public void delete(int no){
+        HeroNode temp = head;
+        if (head.next == null){
+            System.out.println("空链表，删除失败");
+            return;
+        }
+        boolean flag = false;
+        while (true){
+            if (temp.next == null){
+                break;
+            }
+            if (temp.next.no == no){
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag){
+            temp.next = temp.next.next;
+            System.out.printf("已成功删除编号%d的英雄节点\n", no);
+        }else {
+            System.out.printf("找不到编号%d的英雄节点,删除失败\n", no);
+        }
+    }
+
     //显示链表
     public void list(){
         if (head.next == null){
@@ -63,9 +160,9 @@ class SingleLinkedListDemo{
  * 定义HeroNode，每一个HeroNode就是一个节点
  */
 class HeroNode{
-    private int no;
-    private String name;
-    private String nickName;
+    public int no;
+    public String name;
+    public String nickName;
     public HeroNode next;
 
     public HeroNode(int no, String name, String nickName){
