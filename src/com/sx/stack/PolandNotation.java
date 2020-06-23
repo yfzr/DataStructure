92b5b3bb27195f878045bc4221063ca3ed058b29
six1 <six1@chinaunicom.cn>
2020-06-23 17:16:34 +0800

    逆波兰计算器实现2

--- src/com/sx/stack/PolandNotation.java
+++ src/com/sx/stack/PolandNotation.java
@@ -1,6 +1,7 @@
 package com.sx.stack;
 
 import com.sun.javafx.css.PseudoClassState;
+import com.sun.org.apache.bcel.internal.generic.BREAKPOINT;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +11,71 @@ public class PolandNotation {
     public static void main(String[] args){
         //逆波兰表达式
         //String suffixExpression = "30 4 + 5 * 6 -";
-        String suffixExpression = "4 5 * 8 - 60 + 8 2 / +";
-        List<String> list = getListString(suffixExpression);
-        System.out.println("list=" + list);
-        int res = calculate(list);
-        System.out.println("计算结果是：" + res);
+//        String suffixExpression = "4 5 * 8 - 60 + 8 2 / +";
+//        List<String> list = getListString(suffixExpression);
+//        System.out.println("list=" + list);
+//        int res = calculate(list);
+//        System.out.println("计算结果是：" + res);
+
+        String expression = "1+((2+3)*4)-5";
+        List<String> list1 = infixToList(expression);
+        System.out.println(list1);
+
+        List<String> list2 = parseSuffixList(list1);
+        System.out.println("中缀转后缀：" + list2);
+    }
+
+    //将中缀表达式存放在List中
+    public static List<String> infixToList(String expression){
+        List<String> list = new ArrayList<>();
+        int index = 0;
+        char c;
+        String temp;
+        do {
+            //判断该字符是否非数字
+            if ((c = expression.charAt(index)) < 48 || (c = expression.charAt(index)) > 57){
+                list.add("" + c);
+                index++;
+            }else {
+                temp = "";
+                while (index < expression.length() && (c = expression.charAt(index)) >= 48 && (c = expression.charAt(index)) <=57){
+                    temp += c;
+                    index++;
+                }
+                list.add(temp);
+            }
+        }while (index < expression.length());
+        return list;
+    }
+
+    //将中缀表达式转换为后缀表达式，仍然放在List中
+    public static List<String> parseSuffixList(List<String> list){
+        //s1为符号栈
+        Stack<String> s1 = new Stack<>();
+        //由于另一个栈实际上没有出栈的操作，而且最后如果根据栈结构要进行逆序打印，所以可用List代替
+        List<String> s2 = new ArrayList<>();
+        for (String s: list) {
+            if (s.matches("\\d+")){
+                s2.add(s);
+            }else if (s.equals("(")){
+                s1.push(s);
+            }else if (s.equals(")")){
+                while (!s1.peek().equals("(")){
+                    s2.add(s1.pop());
+                }
+                //弹出最后的“（”
+                s1.pop();
+            }else {
+                while (s1.size() > 0 && Operation.getValue(s1.peek()) >= Operation.getValue(s)){
+                    s2.add(s1.pop());
+                }
+                s1.push(s);
+            }
+        }
+        while (s1.size() > 0){
+            s2.add(s1.pop());
+        }
+        return s2;
     }
 
     //将一个逆波兰表达式的数据和符号依次存入List中
@@ -53,4 +114,35 @@ public class PolandNotation {
         }
         return Integer.parseInt(stack.pop());
     }
+}
+
+//解决操作符优先级对比问题
+class Operation{
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 1;
+    private static int DIV = 1;
+    //不同操作符对应不同的优先级数字
+    public static int getValue(String operation){
+        int res = 0;
+        switch (operation){
+            case "+":
+                res = ADD;
+                break;
+            case "-":
+                res = SUB;
+                break;
+            case "*":
+                res = MUL;
+                break;
+            case "/":
+                res = DIV;
+                break;
+            default:
+                System.out.println("异常的运算符");
+                break;
+        }
+        return res;
+    }
+
 }
